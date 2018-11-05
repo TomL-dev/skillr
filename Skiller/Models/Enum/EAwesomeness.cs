49fc3c257d411196520cea15b2dc54c889c2d0af
Tom <tomliefheid@gmail.com>
2018-11-05 21:00:18 +0100

    adding enums

---
+++ Skiller/Models/Enum/EAwesomeness.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Skiller.Models.Enum
+{
+	public enum EAwesomeness
+	{
+		meh = 0,
+		ok_ish = 1,
+		decent = 5,
+		hero = 10,
+		mad_skillz = 42,
+		legendary = 1337,
+		over_9000 = int.MaxValue
+	}
+}
