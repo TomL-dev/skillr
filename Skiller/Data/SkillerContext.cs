38b8432ba34a659cadb299bf192f12a22e442f1a
Tom <tomliefheid@gmail.com>
2018-11-05 21:11:57 +0100

    adding controllers for models

---
+++ Skiller/Data/SkillerContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Skiller.Models;
+
+namespace Skiller.Models
+{
+    public class SkillerContext : DbContext
+    {
+        public SkillerContext (DbContextOptions<SkillerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Skiller.Models.Company> Company { get; set; }
+
+        public DbSet<Skiller.Models.Employee> Employee { get; set; }
+
+        public DbSet<Skiller.Models.MadSkill> MadSkill { get; set; }
+
+        public DbSet<Skiller.Models.Project> Project { get; set; }
+
+        public DbSet<Skiller.Models.User> User { get; set; }
+    }
+}
